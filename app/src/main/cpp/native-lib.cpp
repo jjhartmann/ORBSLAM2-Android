98c460912e7c41abc729acc206b0325f3c26296a
jhartman <jhartman@sfu.ca>
2017-03-13 11:12:02 -0400

    Add them to manifest

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,10 +10,12 @@ Java_org_jjhartmann_jeremy_testopencv2_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
 
     cv::Mat test = cv::Mat::eye(5, 5, CV_64F);
-    cv::VideoCapture camera;
-    camera.open(0);
+    cv::VideoCapture camera(0);
+    bool open = camera.isOpened();
     cv::Mat img;
     camera >> img;
+    cv::Mat frame;
+    cv::cvtColor(img, frame, CV_BGR2GRAY);
     camera.release();
 
 
