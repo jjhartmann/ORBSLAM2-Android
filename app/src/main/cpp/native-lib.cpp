4ba46e846f84178f66ffad530527b4d55a8b8f9f
jhartman <jhartman@sfu.ca>
2017-03-13 14:45:09 -0400

    Add opencv views to activity

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -12,12 +12,13 @@ Java_org_jjhartmann_jeremy_testopencv2_MainActivity_stringFromJNI(
     cv::Mat test = cv::Mat::eye(5, 5, CV_64F);
     cv::VideoCapture camera(0);
     bool open = camera.isOpened();
-    cv::Mat img;
-    camera >> img;
-    cv::Mat frame;
-    cv::cvtColor(img, frame, CV_BGR2GRAY);
-    camera.release();
-
+    if (open) {
+        cv::Mat img;
+        camera >> img;
+        cv::Mat frame;
+        cv::cvtColor(img, frame, CV_BGR2GRAY);
+        camera.release();
+    }
 
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/org/jjhartmann/jeremy/testopencv2/MainActivity.java
+++ app/src/main/java/org/jjhartmann/jeremy/testopencv2/MainActivity.java
@@ -2,16 +2,48 @@ package org.jjhartmann.jeremy.testopencv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2
+{
+    private static final String TAG = "VIO_UW_SAR::Activity";
     // Used to load the 'native-lib' library on application startup.
     static
     {
         System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
     }
+
+    // Call back for async opencv init
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+
+                } break;
+                default:
+                {
+                    Log.i(TAG, "OpenCV Loading error!! - mLoaderCallback");
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,9 +51,24 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = (TextView) findViewById(R.id.textView);
+//        if (!OpenCVLoader.initDebug()) {
+//            textView.setText(textView.getText() + "\n OpenCVLoader.initDebug(), not working.");
+//        } else {
+//            textView.setText(textView.getText() + "\n OpenCVLoader.initDebug(), WORKING.");
+//            //DRS 20160822c Added 1
+//            // textView.setText(textView.getText() + "\n" + validate(0L, 0L));
+//        }
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
     }
 
     /**
@@ -29,4 +76,46 @@ public class MainActivity extends AppCompatActivity
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
+
+    /**********************************************************************************************
+     *  Override methods that implement the CvCameraViewListener2
+     **********************************************************************************************/
+
+    /**
+     * This method is invoked when camera preview has started. After this method is invoked
+     * the frames will start to be delivered to client via the onCameraFrame() callback.
+     *
+     * @param width  -  the width of the frames that will be delivered
+     * @param height - the height of the frames that will be delivered
+     */
+    @Override
+    public void onCameraViewStarted(int width, int height)
+    {
+
+    }
+
+    /**
+     * This method is invoked when camera preview has been stopped for some reason.
+     * No frames will be delivered via onCameraFrame() callback after this method is called.
+     */
+    @Override
+    public void onCameraViewStopped()
+    {
+
+    }
+
+    /**
+     * This method is invoked when delivery of the frame needs to be done.
+     * The returned values - is a modified frame which needs to be displayed on the screen.
+     * TODO: pass the parameters specifying the format of the frame (BPP, YUV or RGB and etc)
+     *
+     * @param inputFrame
+     */
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame)
+    {
+        return null;
+    }
 }
