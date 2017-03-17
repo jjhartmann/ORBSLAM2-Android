d62fa04515ab06d030b7ac05381d6e1d4184de3e
jhartman <jhartman@sfu.ca>
2017-03-16 20:34:57 -0400

    Update Gradel properties.
    Added offline and deamon builds

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,7 +6,8 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
 #include <opencv2/core/ocl.hpp>
-
+#include <boost/tuple/tuple.hpp>
+#include <boost/tuple/tuple_io.hpp>
 
 using namespace std;
 using namespace cv;
@@ -19,15 +20,10 @@ Java_org_jjhartmann_jeremy_testopencv2_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
 
     cv::Mat test = cv::Mat::eye(5, 5, CV_64F);
-    cv::VideoCapture camera(0);
-    bool open = camera.isOpened();
-    if (open) {
-        cv::Mat img;
-        camera >> img;
-        cv::Mat frame;
-        cv::cvtColor(img, frame, CV_BGR2GRAY);
-        camera.release();
-    }
+
+    typedef boost::tuple<std::string, int> animal;
+    animal a{"cat", 4};
+
 
     return env->NewStringUTF(hello.c_str());
 }
