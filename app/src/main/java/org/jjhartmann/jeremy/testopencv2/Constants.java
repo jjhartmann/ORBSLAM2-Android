249bbe10caa95cff33e0e74bed29518d6bfda20e
jhartman <jhartman@sfu.ca>
2017-03-20 01:05:01 -0400

    Add Constants class to manage all constant variables

---
+++ app/src/main/java/org/jjhartmann/jeremy/testopencv2/Constants.java
@@ -0,0 +1,15 @@
+package org.jjhartmann.jeremy.testopencv2;
+
+/**
+ * VisualInertialOdometry-Android-UW-SAR-2
+ * <p>
+ * Created by J. J. Hartmann on 3/19/2017.
+ * Email: j3hartma@uwaterloo.ca
+ * Copyright 2017
+ */
+
+public class Constants
+{
+    static final String CAMERA_CALIBRATION_FILE_NAME = "CameraCalibration.yaml";
+    static final String ORBBOW_VOCABULARY_FILE_NAME = "ORBBoWVocabulary.txt";
+}
